e8c2e91b0234c77ec25ebdbbc067f3c292ea7e5a
Mofassil <mofassilsaiyad.180@gmail.com>
2024-11-30 18:51:38 +0530

    logout section completed

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { body } = require("express-validator")
 const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
+//use to register user
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({ min: 3}).withMessage('First name must be at least 3 characters long'),
@@ -11,13 +13,19 @@ router.post('/register',[
     userController.registerUser
 )
 
-
+//use to login user
 router.post('/login',[
     body('email').isEmail().withMessage('Invalid Email'),
     body('password').isLength({min: 6}).withMessage('Password must be at least 6 character long')
 ],
     userController.loginUser
 )
+
+
+//use to open profile
+router.get('/profile',authMiddleware.authUser, userController.getUserProfile)
+
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 
 module.exports = router;
